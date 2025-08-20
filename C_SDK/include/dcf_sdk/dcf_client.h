9c9ac3851273b8bd4c29a0d9a8ae2c72d7f78fd4
Asher <146795551+ALH477@users.noreply.github.com>
2025-08-19 21:27:48 -0700

    Update dcf_client.h

--- C_SDK/include/dcf_sdk/dcf_client.h
+++ C_SDK/include/dcf_sdk/dcf_client.h
@@ -1,30 +1,20 @@
-
 #ifndef DCF_CLIENT_H
 #define DCF_CLIENT_H
-
-#include <stdbool.h>
-#include <stdint.h>
 #include "dcf_config.h"
 #include "dcf_networking.h"
 #include "dcf_redundancy.h"
 #include "dcf_plugin_manager.h"
 #include "dcf_error.h"
 
-typedef struct {
-    DCFConfig* config;
-    DCFNetworking* net;
-    DCFRedundancy* redundancy;
-    DCFPluginManager* plugin_mgr;
-    bool running;
-} DCFClient;
+typedef enum { CLIENT_MODE, SERVER_MODE, P2P_MODE } DCFMode;
+
+typedef struct DCFClient DCFClient;
 
 DCFClient* dcf_client_new(void);
-DCFError dcf_client_free(DCFClient* client);
 DCFError dcf_client_initialize(DCFClient* client, const char* config_path);
 DCFError dcf_client_start(DCFClient* client);
 DCFError dcf_client_stop(DCFClient* client);
-DCFError dcf_client_send_message(DCFClient* client, const char* data, const char* recipient);
-DCFError dcf_client_receive_message(DCFClient* client, char** message_out);
-
+DCFError dcf_client_send_message(DCFClient* client, const char* data, const char* recipient, char** response_out);
+DCFError dcf_client_receive_message(DCFClient* client, char** message_out, char** sender_out);
+void dcf_client_free(DCFClient* client);
 #endif
-
