16987684197083a9b23125c1c35582bb1ef3918f
Asher <146795551+ALH477@users.noreply.github.com>
2025-08-20 01:33:07 -0700

    Update dcf_redundancy.h

--- C_SDK/include/dcf_sdk/dcf_redundancy.h
+++ C_SDK/include/dcf_sdk/dcf_redundancy.h
@@ -8,9 +8,11 @@ typedef struct DCFRedundancy DCFRedundancy;
 
 DCFRedundancy* dcf_redundancy_new(void);
 DCFError dcf_redundancy_initialize(DCFRedundancy* redundancy, DCFConfig* config, DCFNetworking* networking);
-DCFError dcf_redundancy_start(DCFRedundancy* redundancy);
+DCFError dcf_redundancy_start(DCFRedundancy* redundancy, DCFMode mode);
 DCFError dcf_redundancy_stop(DCFRedundancy* redundancy);
 DCFError dcf_redundancy_get_optimal_route(DCFRedundancy* redundancy, const char* recipient, char** route_out);
 DCFError dcf_redundancy_health_check(DCFRedundancy* redundancy, const char* peer, int* rtt_out);
+DCFError dcf_redundancy_simulate_failure(DCFRedundancy* redundancy, const char* peer);
+DCFError dcf_redundancy_group_peers(DCFRedundancy* redundancy);
 void dcf_redundancy_free(DCFRedundancy* redundancy);
 #endif
